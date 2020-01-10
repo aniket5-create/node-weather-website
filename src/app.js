e10daa019cdd39922293a335e312322802c260e8
aniket <jainaniket442@gmail.com>
2020-01-10 18:38:02 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utilis/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
 const partialsPath = path.join(__dirname,'../templates/partials')
@@ -88,6 +90,6 @@ app.get('*',(req,res) => {
     })
 });
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('server is up on port 3000');
 });
